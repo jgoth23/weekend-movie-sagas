eee5939d9cfbd60cf131ae75102a395867ab0aeb
Johnny Goth <Jgoth23@gmail.com>
2021-03-07 12:15:06 -0600

    details are moving to page

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -1,16 +1,31 @@
 import React, { useEffect, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 function Details() {
   const dispatch = useDispatch();
 
+const details = useSelector((store) => store.details);
 
 
-
-
+// returned 0 to avoid changing the query 
   return (
     <div>
       <h1>All the deets!</h1>
+      <div>
+        <h2>{details[0].title}</h2>
+        <img src={details[0].poster}/>
+        <p>{details[0].description}</p>
+        {details.map((genre) => {
+          return (  <div>
+            <ul>
+            <li>
+              {genre.name} 
+            </li>
+          </ul>
+          </div>
+          )
+        })}
+      </div>
       <button>Back to Home</button>
     </div>
   )
