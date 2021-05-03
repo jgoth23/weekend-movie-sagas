b8eff9a5ba6384b74b0360971405b5351972dd8f
Johnny Goth <Jgoth23@gmail.com>
2021-05-03 15:49:09 -0500

    brought in icons

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -10,13 +10,6 @@ import { purple} from '@material-ui/core/colors';
 import Typography from '@material-ui/core/Typography';
 import Container from '@material-ui/core/Container';
 import swal from 'sweetalert';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 
 
 const useStyles = makeStyles({
