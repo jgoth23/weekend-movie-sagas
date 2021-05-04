25f98585bd2383a7192b875a2d8a74e1211d1105
Johnny Goth <Jgoth23@gmail.com>
2021-05-03 19:06:43 -0500

    add movie button pushes to the next page

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,48 +1,51 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import './MovieList.css'
+import './MovieList.css';
 import { makeStyles } from '@material-ui/core/styles';
 
 function MovieList() {
+  const dispatch = useDispatch();
+  const movies = useSelector((store) => store.movies);
+  const history = useHistory();
 
-    const dispatch = useDispatch();
-    const movies = useSelector(store => store.movies);
-     const history = useHistory();
+  useEffect(() => {
+    dispatch({ type: 'FETCH_MOVIES' });
+  }, []);
 
+  const movieInfo = (id) => {
+    console.log('made it');
+    dispatch({ type: 'FETCH_DETAILS', payload: id });
+    history.push('/details');
+  };
 
-    useEffect(() => {
-        dispatch({ type: 'FETCH_MOVIES' });
-    }, []);
-     
-    const movieInfo = (id) => {
-      console.log('made it');
-      dispatch({type: 'FETCH_DETAILS', payload: id})
-      history.push('/details');
-      
-      
-    }
+  const addMovie = () => {
+    history.push('/addmovie')
+  }
 
-    return (
-        <main>
-            <h1>MovieList</h1>
-            
-            <section className="movies">
-                {movies.map(movie => {
-                    return (
-                        <div className="divContainer" key={movie.id} >
-                            <h3>{movie.title}</h3>
-                            <img onClick={() => movieInfo(movie.id)} src={movie.poster} alt={movie.title}/>
-                        </div>
-                    );
-                })}
-                <div>
-                  <button>Add Movie</button>
-                </div>
-            </section>
-        </main>
+  return (
+    <main>
+      <h1>MovieList</h1>
 
-    );
+      <section className="movies">
+        {movies.map((movie) => {
+          return (
+            <div className="divContainer" key={movie.id}>
+              <h3>{movie.title}</h3>
+              <img
+                onClick={() => movieInfo(movie.id)}
+                src={movie.poster}
+                alt={movie.title}
+              />
+            </div>
+          );
+        })}
+      </section>
+      <div>
+        <button onClick={addMovie}>Add Movie</button>
+      </div>
+    </main>
+  );
 }
 
 export default MovieList;
