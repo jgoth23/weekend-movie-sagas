1888d40f16ff959d651f96e2e22e2887939e5cc5
Johnny Goth <Jgoth23@gmail.com>
2021-05-04 22:16:32 -0500

    set up delete route

--- server/routes/movie.router.js
+++ server/routes/movie.router.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const pool = require('../modules/pool')
 
 router.get('/', (req, res) => {
-
   const query = `SELECT * FROM movies ORDER BY "title" ASC`;
   pool.query(query)
     .then( result => {
@@ -67,5 +66,16 @@ router.post('/', (req, res) => {
     res.sendStatus(500)
   })
 })
+
+router.delete('/:id', (req, res) => {
+  pool.query('DELETE FROM "movies" WHERE id=$1', [req.params.id])
+  .then((result) => {
+    res.sendStatus(200);
+  })
+  .catch((error) => {
+    console.log('delete error', error );
+    res.sendStatus(500)
+  })
+});
 
 module.exports = router;
